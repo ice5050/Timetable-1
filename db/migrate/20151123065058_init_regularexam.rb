eb05d51ff7af03694e302f7bb5190f46b17e38d8
Pondpaun7z <pppangpondpp7@gmail.com>
2015-11-23 14:09:43 +0700

    Edit database Regular exam

---
+++ db/migrate/20151123065058_init_regularexam.rb
@@ -0,0 +1,49 @@
+class InitRegularexam < ActiveRecord::Migration
+  def change
+    Regularexam.create(ordered: 1, dayexam: "2", timeexam: "7", dateexam: "WED MAY 04 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 2, dayexam: "5", timeexam: "7", dateexam: "WED MAY 04 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+
+    Regularexam.create(ordered: 3, dayexam: "2", timeexam: "10", dateexam: "FRI MAY 06 08:00 - 11:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 4, dayexam: "2", timeexam: "11", dateexam: "FRI MAY 06 08:00 - 11:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 5, dayexam: "5", timeexam: "10", dateexam: "FRI MAY 06 08:00 - 11:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 6, dayexam: "5", timeexam: "11", dateexam: "FRI MAY 06 08:00 - 11:00", yearexam: 58, semesterexam: 2)
+
+    Regularexam.create(ordered: 7, dayexam: "3", timeexam: "7", dateexam: "FRI MAY 06 15:30 - 18:30", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 8, dayexam: "6", timeexam: "7", dateexam: "FRI MAY 06 15:30 - 18:30", yearexam: 58, semesterexam: 2)
+
+    Regularexam.create(ordered: 9, dayexam: "2", timeexam: "1", dateexam: "SAT MAY 07 08:00 - 11:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 10, dayexam: "5", timeexam: "1", dateexam: "SAT MAY 07 08:00 - 11:00", yearexam: 58, semesterexam: 2)
+
+    Regularexam.create(ordered: 11, dayexam: "2", timeexam: "17", dateexam: "SAT MAY 07 15:30 - 18:30", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 12, dayexam: "5", timeexam: "17", dateexam: "SAT MAY 07 15:30 - 18:30", yearexam: 58, semesterexam: 2)
+
+    Regularexam.create(ordered: 13, dayexam: "3", timeexam: "17", dateexam: "MON MAY 09 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 14, dayexam: "6", timeexam: "17", dateexam: "MON MAY 09 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+
+    Regularexam.create(ordered: 15, dayexam: "3", timeexam: "10", dateexam: "WED MAY 11 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 16, dayexam: "3", timeexam: "11", dateexam: "WED MAY 11 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 17, dayexam: "6", timeexam: "10", dateexam: "WED MAY 11 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 18, dayexam: "6", timeexam: "11", dateexam: "WED MAY 11 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+
+    Regularexam.create(ordered: 19, dayexam: "3", timeexam: "1", dateexam: "THU MAY 12 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 20, dayexam: "6", timeexam: "1", dateexam: "THU MAY 12 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+
+    Regularexam.create(ordered: 21, dayexam: "2", timeexam: "4", dateexam: "FRI MAY 13 08:00 - 11:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 22, dayexam: "5", timeexam: "4", dateexam: "FRI MAY 13 08:00 - 11:00", yearexam: 58, semesterexam: 2)
+
+    Regularexam.create(ordered: 23, dayexam: "3", timeexam: "4", dateexam: "FRI MAY 13 15:30 - 18:30", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 24, dayexam: "6", timeexam: "4", dateexam: "FRI MAY 13 15:30 - 18:30", yearexam: 58, semesterexam: 2)
+
+    Regularexam.create(ordered: 25, dayexam: "3", timeexam: "14", dateexam: "SAT MAY 14 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 26, dayexam: "6", timeexam: "14", dateexam: "SAT MAY 14 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+
+    Regularexam.create(ordered: 27, dayexam: "2", timeexam: "14", dateexam: "SUN MAY 15 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 28, dayexam: "5", timeexam: "14", dateexam: "SUN MAY 15 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+
+    Regularexam.create(ordered: 29, dayexam: "4", timeexam: "4", dateexam: "MON MAY 16 08:00 - 11:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 30, dayexam: "4", timeexam: "10", dateexam: "MON MAY 16 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 31, dayexam: "4", timeexam: "11", dateexam: "MON MAY 16 12:00 - 15:00", yearexam: 58, semesterexam: 2)
+    Regularexam.create(ordered: 32, dayexam: "4", timeexam: "14", dateexam: "MON MAY 16 15:30 - 18:30", yearexam: 58, semesterexam: 2)
+  end
+end
+
