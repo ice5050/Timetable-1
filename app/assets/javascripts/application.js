020b9e764bdb07b89c78145ce15a74fe283aae04
pondpaun7z <pppangpondpp7@gmail.com>
2015-12-15 17:39:17 +0700

    blocked Adblock

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,49 +17,7 @@
 // require material
 //= require_tree .
 
-jQuery.adblock = false;
-(function($){
 
-    if ($.adblock === undefined){
-        $.adblock = true;
-    }
-
-    $.fn.showOnAdBlock = function(){
-
-        if($.adblock){
-            this.show();
-        }
-
-        return this;
-    };
-
-})(jQuery);
-
-$(function(){
-
-    if($.adblock){
-        $.confirm({
-            'title'     : 'Adblocker active!',
-            'message'   : 'You are running an ..',
-            'buttons'   : {
-                'I will!'   : {
-                    'class' : 'blue',
-                    'action': function(){
-                        // Do nothing
-                        return;
-                    }
-                },
-                'Never!'    : {
-                    'class' : 'gray',
-                    'action': function(){
-                        // Redirect to some page
-                        window.location = 'http://tutorialzine.com/';
-                    }
-                }
-            }
-        });
-    }
-});
 
 
 $(document).ready(function() {
