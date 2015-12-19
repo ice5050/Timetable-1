666ce536d2dd7ff66280f0b0a7e8b227b698f5b6
pondpaun7z <pppangpondpp7@gmail.com>
2015-12-20 01:34:18 +0700

    changed table => div in edit_eaxm_paht, improved homepages_path

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -2,10 +2,19 @@ require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
 
+    setup do 
+        @user = users(:guest)
+        @table = tables(:table_1)
+    end
 
-    test "should get index" do
+    test "should get index current_path => classtatables" do
+        get :index, user_id: @user, id: @table
+        assert_response :redirect
+    end
+
+    test "should get index current_path => other" do
         get :index
-        assert_response :success
+        assert_response :redirect
     end
 
 end
