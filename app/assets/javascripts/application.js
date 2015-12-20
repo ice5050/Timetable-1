27b186f1cd3eeaa6a312cee93892a760906ac1d7
pondpaun7z <pppangpondpp7@gmail.com>
2015-12-20 18:28:44 +0700

    edited share button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,27 @@
 // require turbolinks
 //= require_tree .
 
+$(".share-popup").click(function(){
+    var window_size = "";
+    var url = this.href;
+    var domain = url.split("/")[2];
+    switch(domain) {
+        case "www.facebook.com":
+            window_size = "width=585,height=368";
+            break;
+        case "www.twitter.com":
+            window_size = "width=585,height=261";
+            break;
+        case "plus.google.com":
+            window_size = "width=517,height=511";
+            break;
+        default:
+            window_size = "width=585,height=511";
+    }
+    window.open(url, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,' + window_size);
+    return false;
+});
+
 $(document).ready(function() {
     $('#new').click(function() {
         $('this').hide();
