09d2d34da475fa3c0835d2b7298d08eb9b5a6268
pondpaun7z <pppangpondpp7@gmail.com>
2015-12-21 21:54:08 +0700

    edit generate_controller_test

--- test/controllers/generates_controller_test.rb
+++ test/controllers/generates_controller_test.rb
@@ -6,12 +6,12 @@ class GeneratesControllerTest < ActionController::TestCase
     end
 
     test "should get index without login" do       
-        get :index
+        get :index, semester: 1, year: 2, stu_id: 570510659
         assert_response :success
     end
 
     test "should get index with login" do       
-        get :index, id: @user
+        get :index, id: @user, semester: 1, year: 2, stu_id: 570510659
         assert_response :success
     end
 
