3cb99e32628ebc03fb1798135e790c9e23bebfb8
pondpaun7z <pppangpondpp7@gmail.com>
2015-12-30 00:17:24 +0700

    Edited UI homepage, Edited text 'add to my table'

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -2,4 +2,16 @@ class HomepagesController < ApplicationController
     def index
         @users = User.all
     end
+    
+    def resource_name
+        :user
+    end
+
+    def resource
+        @resource ||= User.new
+    end
+
+    def devise_mapping
+        @devise_mapping ||= Devise.mappings[:user]
+    end
 end
--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class StudentsControllerTest < ActionController::TestCase
   test "should get index" do
-    get :index
+    user = users(:admin)
+    url = "https://www3.reg.cmu.ac.th/regist258/public/stdtotal.php?var=add&COURSENO=204100&SECLEC=001&SECLAB=000"
+    get :index, user_id: user, url: url, code: 204100
     assert_response :success
   end
 
