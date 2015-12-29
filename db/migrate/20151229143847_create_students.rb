59ff7d587336a81f4396a4ec0c256d6e5c0d3449
pondpaun7z <pppangpondpp7@gmail.com>
2015-12-30 00:24:39 +0700

    removed students table

--- db/migrate/20151229143847_create_students.rb
+++
@@ -1,9 +0,0 @@
-class CreateStudents < ActiveRecord::Migration
-  def change
-    create_table :students do |t|
-        t.string :stuId
-        t.string :urlImg
-        t.timestamps null: false
-    end
-  end
-end
